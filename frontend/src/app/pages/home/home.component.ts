26c89c4e678b0758c40aee47274486b668430ba0
Roman Nikolaev <r.nikolaev@tn-dl.ru>
2023-11-22 23:23:13 +0300

    add temporary queries for create entities

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,27 @@
 import { Component } from '@angular/core';
+import { catchError, of, take } from 'rxjs';
+import { Directory } from 'src/app/interfaces/directory';
+import { BrandsService } from 'src/app/services/directories/brands/brands.service';
+import { ColorsService } from 'src/app/services/directories/colors/colors.service';
+import { TypesService } from 'src/app/services/directories/types/types.service';
+
+const types = [
+  'ABS',
+  'ABS+',
+  'ASA',
+  'FLEX',
+  'HIPS',
+  'Nylon',
+  'PCL',
+  'PETG',
+  'PLA',
+  'PLA+',
+  'POM',
+  'PVA',
+  'SBS',
+  'SLA',
+  'TPU',
+];
 
 @Component({
   selector: 'app-home',
@@ -6,6 +29,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
   constructor(
+    private brandsService: BrandsService,
+    private colorsService: ColorsService,
+    private typesService: TypesService,
   ) {}
+
+  // createBrand() {
+  //   this.loading = true;
+  //   brands.forEach((brand, index) => {
+  //     setTimeout(() => {
+  //       this.brandsService.createBrand(brand).pipe(
+  //         take(1),
+  //         catchError(err => {
+  //           console.error(err);
+  //           return of(null);
+  //         })
+  //       ).subscribe(res => {
+  //         console.log(`${brand} created, id = ${(res?.data as any).id}`);
+  //       });
+  //     }, 200);
+  //   });
+  // }
+
+  create() {
+    types.forEach((type, index) => {
+      setTimeout(() => {
+        this.typesService.createType(type).pipe(
+          take(1),
+          catchError(err => {
+            console.error(err);
+            return of(null);
+          })
+        ).subscribe(res => {
+          console.log(`${type} created, id = ${(res?.data as any).id}`);
+        });
+      }, 200);
+    });
+  }
 }
--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -2,5 +2,6 @@ import { Environment } from "src/app/interfaces/environment";
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://localhost:1337/api',
+  // apiUrl: 'http://localhost:1337/api',
+  apiUrl: 'http://62.113.110.106:1337/api',
 };
